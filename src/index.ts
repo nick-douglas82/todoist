db7fccdcc615dd6372b3b0d658c2b1bd2a12d887
Nick Douglas <nick.douglas82@gmail.com>
2021-04-21 10:00:09 +0100

    Setting up GQL

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,31 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Task } from './entities/Task';
-import mircoConfig from "./mikro-orm.config";
+import { Task } from "./entities/Task";
+import microConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
-  const orm = await MikroORM.init(mircoConfig);
+  const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
-  const task = orm.em.create(Task, {title: 'This is a test task'});
-  await orm.em.persistAndFlush(task);
-}
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+  });
+};
 
 main().catch((err) => {
   console.error(err);
