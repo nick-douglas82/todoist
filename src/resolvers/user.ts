50dfecd5e80034dd4e2b9c41f7ff31fea2407924
Nick Douglas <nick.douglas82@gmail.com>
2021-04-26 10:08:52 +0100

    Adding "me" query

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,4 +1,4 @@
-import { Resolver, Mutation, Arg, InputType, ObjectType, Field, Ctx } from "type-graphql";
+import { Resolver, Mutation, Arg, InputType, ObjectType, Field, Ctx, Query } from "type-graphql";
 import argon2 from "argon2";
 import { MyContext } from "../types";
 import { User } from "../entities/User";
@@ -29,10 +29,24 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
+  @Query(() => User, { nullable: true })
+  async me (
+    @Ctx() { req, em }: MyContext
+  ) {
+
+    // You are not logged in
+    if (!req.session.userId) {
+      return null;
+    }
+
+    const user = await em.findOne(User, {id: req.session.userId})
+    return user;
+  }
+
   @Mutation(() => UserResponse)
   async register(
     @Arg("options") options: UsernamePasswordInput,
-    @Ctx() { em }: MyContext
+    @Ctx() { em, req }: MyContext
   ): Promise<UserResponse> {
     if (options.username.length <= 2) {
       return {
@@ -65,13 +79,16 @@ export class UserResolver {
           errors: [
             {
               field: "username",
-              message: "username already taken"
+              message: "username already taken" //2:03:09
             }
           ]
         }
       }
     }
-    return { user }; // 1:35:24
+    // Store user id session and auto log the user in
+    req.session.userId = user.id;
+
+    return { user };
   }
 
   @Mutation(() => UserResponse)
